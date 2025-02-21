85f1001a26950afb8789a1829eb75b370f60dd11
Amr Koritem <amr.koritem92@gmail.com>
2025-02-21 21:38:31 +0200

    [feat] tabbar created

---
+++ lib/presentation/screens/tabbar_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class TabbarScreen extends StatefulWidget {
+  const TabbarScreen({super.key});
+
+  @override
+  State<TabbarScreen> createState() => _TabbarScreenState();
+}
+
+class _TabbarScreenState extends State<TabbarScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> tabs = <Widget>[
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.yellow,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: tabs.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
